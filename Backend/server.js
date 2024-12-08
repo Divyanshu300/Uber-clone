e2698305706b1715e7171f6d1d45f69f88ddbfd2
Divyanshu800 <pathakdivyanshu800@gmail.com>
2024-12-08 18:55:09 +0530

    Created user schema and methods

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ const http = require("http");
 const app = require("./index");
 const port = process.env.PORT || 3000;
 
+//JB BHI HMM LOG SOCKET KA USE KRTE HAI TB HMMLOG http KA USE KRTE HAI TO CREATE SERVER
 const server = http.createServer(app);
 
 server.listen(port , () => {
